a9fab229fec9e857b2adb0a19dba9e6ff964a639
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-08 01:51:20 +0000

    Adding tarfile member sanitization to extractall()

--- src/davai_env/shelf.py
+++ src/davai_env/shelf.py
@@ -58,7 +58,26 @@ class Shelf(object):
         """Extracts a tar/tgz shelf into marketplacecache."""
         assert os.path.exists(self.tarfile)
         with tarfile.open(self.tarfile, 'r') as t:
-            t.extractall(path=self.rootdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(t, path=self.rootdir)
 
     def _mkt_arch(self, archive, to='arch'):
         if to == 'arch':
